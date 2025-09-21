d39c0d931292211f915e6694303e22454a70142f
raojiajun <471412450@qq.com>
2025-09-21 11:00:18 +0800

    feat: init

--- apps/count-number/src/source/_components/number-action/index.tsx
+++ apps/count-number/src/source/_components/number-action/index.tsx
@@ -1,12 +1,19 @@
 import React, {useEffect} from "react";
 import {useGetCountNumber} from "@/source/_hooks/useGetCountNumber";
+import { Button } from "@/components/ui/button"
+
 
 function NumberAction({data}: {data: number}){
     const {data: testData} = useGetCountNumber()
     console.log('testData', testData)
     console.log('data', data)
+    const test = ()=>{
+        console.log(111)
+    }
     return (
-        <div>number action</div>
+        <div>
+            <Button onClick={test}>test</Button>
+        </div>
     )
 }
 export default NumberAction
