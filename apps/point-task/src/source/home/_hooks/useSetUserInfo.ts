e7aed0315c4fca1dd376190c107227d8a158db60
rao <1941731228@qq.com>
2025-10-30 14:45:39 +0800

    init

---
+++ apps/point-task/src/source/home/_hooks/useSetUserInfo.ts
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import { useAppStore } from "../_store";
+
+function useSetUserInfo() {
+    const setUserInfo = useAppStore(state => state.setUserInfo)
+
+    useEffect(()=>{
+
+        setUserInfo({ userId: "user001" })
+    }, [])
+}
--- apps/point-task/src/source/home/_store/index.ts
+++ apps/point-task/src/source/home/_store/index.ts
@@ -1,13 +1,11 @@
 import { create } from 'zustand'
-import { Category } from '@/source/home/_api/get-category'
-import { Banner } from '@/source/home/_api/get-banner'
 
 interface AppStore {
-  countNumber: number
-  setCountNumber: (newNumber: number) => void
+  userInfo: Record<string, any>
+  setUserInfo: (info: Record<string, any>) => void
 }
 
 export const useAppStore = create<AppStore>(set => ({
-  countNumber: 100,
-  setCountNumber: (newNumber: number) => set({ countNumber: newNumber }),
+  userInfo: {},
+  setUserInfo: (info) => set({ userInfo: info }),
 }))
