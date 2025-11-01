76557630537262b4353e98988c70312284f54870
rao <1941731228@qq.com>
2025-11-01 21:23:08 +0800

    home-样式

--- apps/point-task/src/source/home/_components/ChoiceArea/index.tsx
+++ apps/point-task/src/source/home/_components/ChoiceArea/index.tsx
@@ -14,21 +14,21 @@ function ChoiceArea() {
       <div className='flex justify-center items-center mb-6'>
         <div className='grid grid-cols-2 gap-3 h-[45vh] w-[70vh] bg-gray-50 text-white px-6 py-6 rounded-lg mb-6 drop-shadow-2xl'>
           <div
-            className='bg-blue-500/100 flex flex-col justify-center items-center rounded-lg cursor-pointer hover:bg-sky-600/100 transition-colors'
+            className=' bg-blue-500/100 flex flex-col justify-center items-center rounded-lg cursor-pointer hover:bg-blue-600/100 transition-colors'
             onClick={() => setShowSetGoals(true)}
           >
             <FaBullseye className='text-white text-xl mb-2' />
             <div>创建目标</div>
           </div>
-          <div className='bg-green-500/100 flex flex-col justify-center items-center rounded-lg cursor-pointer hover:bg-sky-5/100 transition-colors '>
+          <div className='bg-green-500/100 flex flex-col justify-center items-center rounded-lg cursor-pointer hover:bg-green-600/100 transition-colors '>
             <FaPlus className='text-white text-xl' />
             <div>添加任务</div>
           </div>
-          <div className='bg-orange-500/100 flex flex-col justify-center items-center rounded-lg cursor-pointer hover:bg-green-600/100 transition-colors '>
+          <div className='bg-orange-500/100 flex flex-col justify-center items-center rounded-lg cursor-pointer hover:bg-orange-600/100 transition-colors '>
             <FaCheck className='text-white text-xl mb-2' />
             <div>完成任务</div>
           </div>
-          <div className='bg-purple-500/100  flex flex-col justify-center items-center rounded-lg cursor-pointer hover:bg-sky-600/100 transition-colors '>
+          <div className='bg-purple-500/100  flex flex-col justify-center items-center rounded-lg cursor-pointer hover:bg-purple-600/100 transition-colors '>
             <FaGift className='text-white text-xl mb-2' />
             <div>兑换奖励</div>
           </div>
