d155a29322b164a0cf7e2dc01e521d1a58e7e7cb
rao <1941731228@qq.com>
2025-11-15 10:23:27 +0800

    重写

--- apps/point-task/src/source/home/page.tsx
+++ apps/point-task/src/source/home/page.tsx
@@ -1,18 +1,5 @@
-import ActionMenu from './_components/ActionMenu'
-import DashboardHeader from './_components/DashboardHeader'
-import useUserProfile from './_hooks/useUserProfile'
-import { useUserTargetsHooks } from './_hooks/useUserTargets'
-
 function Page() {
-  useUserProfile()
-  useUserTargetsHooks()
-
-  return (
-    <div>
-      <DashboardHeader></DashboardHeader>
-      <ActionMenu></ActionMenu>
-    </div>
-  )
+  return <div>test</div>
 }
 
 export default Page
