82ed8d68252f92f0d5bd2216593d1b32028f2c95
rao <1941731228@qq.com>
2025-11-20 15:10:44 +0800

    userInfo-complete

--- apps/point-task/src/source/home/_components/UserInfo/index.tsx
+++ apps/point-task/src/source/home/_components/UserInfo/index.tsx
@@ -3,8 +3,14 @@ import { useAppStore } from '../../_store'
 
 function GetUserInfo() {
   const { userInfo } = useAppStore()
-
   useGetUserInfoHooks()
+  const { getUserTasksList } = useAppStore()
+
+  const pendingTasksCount = getUserTasksList.filter(
+    item => !item.is_completed
+  ).length
+
+  console.log('pendingTasksCount', pendingTasksCount)
   return (
     <div className='bg-blue-700 text-white p-6 w-2/5 mx-auto rounded-lg shadow-lg'>
       {/* head */}
@@ -17,7 +23,9 @@ function GetUserInfo() {
       </div>
 
       {/* todo 等待task和target完成*/}
-      <div className='text-center text-blue-200'>0个目标，0个待完成任务</div>
+      <div className='text-center text-blue-200'>
+        {}个目标，{pendingTasksCount ? pendingTasksCount : 0}个待完成任务
+      </div>
     </div>
   )
 }
